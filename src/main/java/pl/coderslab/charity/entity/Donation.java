b6fd638a66f4891fa29914204f7cccc3f9fe479c
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2023-03-01 14:44:58 +0100

    Done.

--- src/main/java/pl/coderslab/charity/entity/Donation.java
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -34,4 +34,84 @@ public class Donation {
     private LocalTime pickUpTime;
 
     private String pickUpComment;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPickUpTime(LocalTime pickUpTime) {
+        this.pickUpTime = pickUpTime;
+    }
+
+    public void setPickUpDate(LocalDate pickUpDate) {
+        this.pickUpDate = pickUpDate;
+    }
+
+    public void setPickUpComment(String pickUpComment) {
+        this.pickUpComment = pickUpComment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public LocalDate getPickUpDate() {
+        return pickUpDate;
+    }
+
+    public LocalTime getPickUpTime() {
+        return pickUpTime;
+    }
+
+    public String getPickUpComment() {
+        return pickUpComment;
+    }
 }
