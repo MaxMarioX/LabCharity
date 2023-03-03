9ffe5647b0f3c915369081007ea0cc5c2a1a7803
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2023-03-03 21:06:44 +0100

    Done.

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -1,30 +1,56 @@
 package pl.coderslab.charity.controller;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.dao.DonationsDao;
-import pl.coderslab.charity.dao.InstitutionDao;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.charity.repository.DonationRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class HomeController {
 
-    private final InstitutionDao institutionDao;
-    private final DonationsDao donationsDao;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionDao institutionDao, DonationsDao donationsDao)
+    private final InstitutionRepository institutionRepository;
+
+    private JavaMailSender javaMailSender;
+
+    public HomeController(JavaMailSender javaMailSender, DonationRepository donationRepository, InstitutionRepository institutionRepository)
     {
-        this.institutionDao = institutionDao;
-        this.donationsDao = donationsDao;
+        this.javaMailSender = javaMailSender;
+        this.donationRepository = donationRepository;
+        this.institutionRepository = institutionRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model)
     {
-        model.addAttribute("institutions", institutionDao.findAll());
-        model.addAttribute("quantities", donationsDao.countQuantities());
+        model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("quantities", donationRepository.sumQuantities());
 
         return "index";
+    }
+
+    @PostMapping("/sendmail")
+    public String sendMain(HttpServletRequest httpServletRequest)
+    {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom("pm@mp-programs.pl");
+        mailMessage.setTo("mariusz.plaskota@o2.pl");
+        mailMessage.setSubject("New message from site");
+        mailMessage.setText("--- New message from pomagasie.com ---" +
+                            "\nName: " + httpServletRequest.getParameter("name") +
+                            "\nSurname: " + httpServletRequest.getParameter("surname") +
+                            "\nMessage: " + httpServletRequest.getParameter("message"));
+
+        javaMailSender.send(mailMessage);
+
+        return "redirect:/";
     }
 }
